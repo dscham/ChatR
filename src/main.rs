c61c385c55805ef1a03f94f8a7f5a24d53c94a3e
Dominik Schramm <Dominik.Schramm@activesolution.at>
2022-12-21 17:57:32 +0100

    add is_first_run command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
+mod cli;
+mod chat;
+mod state;
+mod discovery;
+
 use std::borrow::{Borrow, BorrowMut};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::str::FromStr;
@@ -13,21 +18,16 @@ use std::thread;
 use chrono;
 use crate::discovery::HostInfo;
 
-mod cli;
-mod chat;
-mod state;
-mod discovery;
-
 fn main() {
     tauri::Builder::default()
         .setup(|tauri_context| {
-
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
             start_discovery,
             get_host_info,
             save_username,
+            is_first_run,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -45,7 +45,7 @@ fn start_discovery() -> Result<(), String> {
 fn get_host_info() -> Result<HostInfo, String> {
     match state::load_host_info() {
         Ok(host_info) => {
-           Ok(host_info)
+            Ok(host_info)
         }
         Err(e) => {
             Err("Could not load host info".to_string())
@@ -56,18 +56,16 @@ fn get_host_info() -> Result<HostInfo, String> {
 #[tauri::command]
 fn save_username(username: String) -> Result<HostInfo, String> {
     let mut host_info;
-    match state::load_host_info() {
-        Ok(loaded_host_info) => {
-            host_info = loaded_host_info;
-            host_info = HostInfo {
-                id: host_info.id,
-                name: username,
-            };
-        }
-        Err(e) => {
-            host_info = HostInfo::new(&username);
-        }
+    if is_first_run() {
+        host_info = HostInfo::new(&username);
+    } else {
+        host_info = state::load_host_info().unwrap();
+        host_info = HostInfo {
+            id: host_info.id,
+            name: username,
+        };
     }
+
     match state::save_host_info(&host_info) {
         Ok(_) => {
             Ok(host_info)
@@ -75,5 +73,13 @@ fn save_username(username: String) -> Result<HostInfo, String> {
         Err(e) => {
             Err("Could not save username".to_string())
         }
+    }
+}
+
+#[tauri::command]
+fn is_first_run() -> bool {
+    match state::load_host_info() {
+        Ok(_) => false,
+        Err(_) => true,
     }
 }
--- ui/index.js
+++ ui/index.js
@@ -6,28 +6,30 @@ let logged_out_title = () => document.getElementById('logged_out');
 let username_output = () => document.getElementById('current_username');
 let username_input = () => document.getElementById('new_username');
 
+document.addEventListener('DOMContentLoaded', () => {
+    updateUsernameOutput();
+});
+
 function changeUsername() {
     invoke('save_username', {username: username_input().value})
         .then(host_info => {
-            showUsername(host_info.name)
+            updateUsernameOutput(host_info.name)
         })
         .catch(err => console.log(err));
 
 }
 
-function showUsername(username) {
+function updateUsernameOutput(username = 'unknown') {
     username_output().textContent = username;
-    logged_out_title().style.display = 'none';
-    logged_in_title().style.display = 'block';
+    if(isFirstRun()) {
+        logged_out_title().style.display = 'block';
+        logged_in_title().style.display = 'none';
+    } else {
+        logged_in_title().style.display = 'none';
+        logged_out_title().style.display = 'block';
+    }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    logged_out_title().style.display = 'block';
-
-    invoke('get_host_info')
-        .then(host_info => {
-            console.log(host_info);
-            showUsername(host_info.name);
-        })
-        .catch(err => console.log(err));
-});
+function isFirstRun() {
+    return invoke('is_first_run').then(result => {return result});
+}
